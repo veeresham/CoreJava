1f74f343c6be5a84a8f1ab23f88923a241f443f3
veeresham <veeres.veeresh@gmail.com>
2015-05-23 23:34:38 +0900

    Initial Commit

---
+++ cache-builder/src/main/java/com/test/cache/Person.java
@@ -0,0 +1,30 @@
+package com.test.cache;
+
+public class Person
+{
+    private String name;
+    
+    private String location;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+    
+    
+}
