12ed5cc9c49374225345b1e1cfc64ebb5bb97006
veeresham <veeres.veeresh@gmail.com>
2015-05-23 23:12:11 +0900

    Initial Commit

---
+++ spring-batch-chunk/src/main/java/com/test/entity/Destination.java
@@ -0,0 +1,15 @@
+package com.test.entity;
+
+public class Destination {
+
+	private String output;
+
+	public String getOutput() {
+		return output;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+	}
+
+}
---
+++ spring-batch-chunk/src/main/java/com/test/entity/Source.java
@@ -0,0 +1,14 @@
+package com.test.entity;
+
+public class Source {
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+}
